b2d39803fe835289df687b485a1857c2f617bd78
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-25 18:05:17 +0300

    feat: add password hashed

--- utils/userAddToDB.php
+++ utils/userAddToDB.php
@@ -6,9 +6,16 @@ $email = filter_var(trim($_POST['inputEmail']), FILTER_SANITIZE_EMAIL);
 $name = filter_var(trim($_POST['inputName']), FILTER_UNSAFE_RAW);
 $pass = filter_var(trim($_POST['inputPass']), FILTER_UNSAFE_RAW);
 
-$sql = "INSERT INTO users (name, email, pass) VALUES('$name', '$email', '$pass')";
+$hashed_password = password_hash($pass, PASSWORD_DEFAULT);
+echo $hashed_password;
 
-mysqli_query($mysql, $sql);
+$sql = "INSERT INTO users (name, email, pass) VALUES('$name', '$email', '$hashed_password')";
+
+if (mysqli_query($mysql, $sql)) {
+    echo "User added successfully.";
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
 
 mysqli_close($mysql);
 
