265e8edb20cb88a3efe458068722752a2fd71b08
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-26 00:26:53 +0300

    feat: ligin user

---
+++ utils/loginToDB.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+include("./connectToDb.php");
+
+$name = filter_var(trim($_POST['inputName']), FILTER_UNSAFE_RAW);
+$pass = filter_var(trim($_POST['inputPass']), FILTER_UNSAFE_RAW);
+
+$hashed_password = password_hash($pass, PASSWORD_DEFAULT);
+
+$sql = "SELECT name, pass FROM users WHERE name = '$name'";
+
+$result = mysqli_query($mysql, $sql);
+
+mysqli_close($mysql);
+
+if ($result) {
+
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        if (password_verify($pass, $row['pass'])) {
+            $userName = $row['name'];
+
+            require './cookieSetter.php';
+            exit();
+        }
+    };
+};
+
+$_SESSION['checkAuthMessage'] = 'Wrong password or username!';
+$_SESSION['alertClass'] = 'alert-danger';
+
+header('Location: ../login.php');
